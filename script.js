03f178ed782822deb5ba39faec2f402fef035517
Ramyr <myrreg@protonmail.com>
2025-07-09 23:00:40 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+let secretNumber = createSecretNumber();
+let score = 20;
+// document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+
+  if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+  } else {
+    score--;
+    if (score < 1) {
+      document.querySelector('.message').textContent = 'You Lost The Game!!';
+    } else {
+      if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too High!';
+      } else {
+        document.querySelector('.message').textContent = 'Too Low!';
+      }
+    }
+    clearInputField();
+  }
+  document.querySelector('.score').textContent = score;
+
+  if (score === 0) {
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = createSecretNumber();
+  console.log(secretNumber);
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  clearInputField();
+});
+
+function createSecretNumber() {
+  return Math.trunc(Math.random() * 20) + 1;
+}
+
+function clearInputField() {
+  document.querySelector('.guess').value = '';
+}
